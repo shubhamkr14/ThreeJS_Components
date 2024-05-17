afebd84b8be8d47553d214375142f43dd2377a16
shubhamkr14 <shubham.kr.14@gmail.com>
2024-05-17 20:40:11 +0530

    fix upload

--- src/js/index.js
+++ src/js/index.js
@@ -8,6 +8,8 @@ document.addEventListener('DOMContentLoaded', () => {});
 window.addEventListener('load', () => {
   const canvas = document.querySelector('#canvas');
 
+
+  
   if (canvas) {
     new Three(document.querySelector('#canvas'));
   }
